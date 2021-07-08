17b4173833287cd7cdac3731103f017266ffba2a
Martin <martinbobrik@gmail.com>
2021-07-08 16:11:41 -0300

    feat (client/login & clieng/register) components working and with initial style

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,48 +1,33 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
-import { supabase } from "../../supabase/supabase";
+import {Container, Typography, Button, TextField, FormControl} from '@material-ui/core';
+
+import { supabase } from '../../supabase/supabase';
 
 export const Login = () => {
   const history = useHistory();
 
-  const [data, setdata] = useState({
-    email: "",
-    password: "",
+  const [data, setData] = useState({
+    email: '',
+    password: '',
   });
 
   function handleOnChange(e) {
-    setdata({
+    setData({
       ...data,
       [e.target.name]: e.target.value,
     });
-  }
+   }
   //! ---------------------------------
   //! en consola porque sale
   // error de email no confirmado
   // error Invalid email or password
   //! ---------------------------------
 
-  /*   const handleSubmit = async (event) => {
-    event.preventDefault();
-    await supabase.auth
-      .signIn({
-        email: data.email,  
-        password: data.password,
-      })
-      .then((response) => {
-        const { error } = response;
-        if (error) return alert(error.message);
-        // se debe cambiar esta ruta por el home del UR
-        return history.push("/logeado");
-      })
-      // no esta haciendo nada el catch
-      .catch((error) => console.log(error));
-  }; */
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    console.log('hit')
     try {
       let info = await supabase.auth.signIn({
         email: data.email,
@@ -50,48 +35,43 @@ export const Login = () => {
       });
 
       if (info.error) return alert(info.error.message);
-      return history.push("/home");
+      return history.push('/home');
     } catch (error) {
       console.log(error);
     }
   };
 
   const singUpRoute = () => {
-    history.push("/register");
+    history.push('/register');
   };
 
   return (
-    <div>
-      <label> Login</label>
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
-          <input
-            name="email"
-            type="text"
-            value={data.email}
-            onChange={handleOnChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            name="password"
-            placeholder="Password"
-            type="password"
-            value={data.password}
-            onChange={handleOnChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-
-      <button type="button" onClick={singUpRoute}>
-        Sing up
-      </button>
-    </div>
+    <Container maxWidth='sm'>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <Typography variant='h3'> LOGIN</Typography>
+              <TextField
+                required
+                label='Email'
+                name='email'
+                type='text'
+                value={data.email}
+                onChange={handleOnChange}
+                />
+              <TextField
+                required
+                label='Password'
+                name='password'
+                type='password'
+                value={data.password}
+                onChange={handleOnChange}
+                />
+              <Button type='submit' variant='contained' color='primary'>Login</Button>
+              <Button variant='contained' color='primary' onClick={singUpRoute}>
+                Sing up
+              </Button>
+          </FormControl>
+        </form>
+    </Container>
   );
 };
--- client/src/components/register/register.jsx
+++ client/src/components/register/register.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router";
-import { supabase } from "../../supabase/supabase";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { supabase } from '../../supabase/supabase';
+
+import {Container, Typography, Button, TextField, FormControl} from '@material-ui/core';
+
 
 export const Register = () => {
   const history = useHistory();
 
   const [data, setData] = useState({
-    email: "",
-    password: "",
-    passwordValidate: "",
+    email: '',
+    password: '',
+    passwordValidate: '',
   });
 
   function handleOnChange(e) {
@@ -38,70 +41,62 @@ export const Register = () => {
 
           if (error) return alert(error.message);
           // se debe cambiar esta ruta por el home del UR
-          alert("congratulations your account has been created");
-          return history.push("/");
+          alert('congratulations your account has been created');
+          return history.push('/');
         })
         .catch((err) => console.log(err));
     } else {
-      alert("Error in Password");
+      alert('Error in Password');
     }
   };
 
-
   const back = () => {
     // aca seria el landing page 
-    history.push("/");
+    history.push('/');
   };
 
   return (
-    <div>
-      <label> Register </label>
+    <Container maxWidth='sm'>
+      <Typography variant='h3'>
+        Register
+      </Typography>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
-          <input
-            name="email"
-            type="text"
+        <FormControl>
+          <TextField
+            required
+            label='Email'
+            name='email'
+            type='text'
             value={data.email}
             onChange={handleOnChange}
-            required
           />
-        </div>
-
-        <div>
-          <label>Password</label>
-          <input
-            name="password"
-            placeholder="Password"
-            type="password"
+          <TextField
+            required
+            label='Password'
+            name='password'
+            type='password'
             value={data.password}
             onChange={handleOnChange}
-            required
           />
-        </div>
-
-        <div>
-          <label>Repeat Password</label>
-          <input
-            name="passwordValidate"
-            placeholder="Repeat password"
-            type="password"
+          <TextField
+            required
+            label='Repeat Password'
+            name='passwordValidate'
+            type='password'
             value={data.passwordValidate}
             onChange={handleOnChange}
-            required
-          />
-          {data.password === data.passwordValidate ||
-          data.passwordValidate === ""
-            ? null
-            : "No coinciden"}
-        </div>
-
-        <button type="submit">Sing up</button>
+            color={data.password === data.passwordValidate ||
+              data.passwordValidate === ''
+              ? 'primary'
+              : 'secondary'}
+            />
+          
+          <Button type='submit' variant='contained' color='primary'>Sing up</Button>
+          <Button onClick={back} variant='contained' color='primary'>
+           Back
+          </Button>
+        </FormControl>
       </form>
-
-      <button onClick={back} type="button">
-        {"volver "}
-      </button>
-    </div>
+    </Container>
   );
 };
