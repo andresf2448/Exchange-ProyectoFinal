135a4c6e5a98c5de83e7b3ca372402ab16ac605a
Martin <martinbobrik@gmail.com>
2021-07-09 16:15:21 -0300

    feat(client/home/settings) recover password, centralized styles, appbar in settings and form validation from javi

--- client/src/components/register/register.jsx
+++ client/src/components/register/register.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import { supabase } from "supabase/supabase";
+import { validate } from "./validate";
 
 import {
   Container,
@@ -13,6 +14,14 @@ import {
 export const Register = () => {
   const history = useHistory();
 
+  const [error, setError] = useState({
+    email: '',
+    password: "",
+    passwordValidate: "",
+    isError: true
+  })
+  const [submit, setSubmit] = useState(false)
+
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -24,6 +33,10 @@ export const Register = () => {
       ...data,
       [event.target.name]: event.target.value,
     });
+    setError(validate({
+      ...data,
+      [event.target.name]: event.target.value
+    }))
   }
 
   const handleSubmit = async (event) => {
@@ -40,15 +53,25 @@ export const Register = () => {
 
           if (error) return alert(error.message);
 
-          alert("Please, we send you a message to your email");
+          alert("congratulations your account has been created");
           return history.push("/");
-        });
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Error in Password");
     }
   };
 
   const back = () => {
     history.push("/");
   };
+  useEffect(() => {
+    if(error.isError) {
+      setSubmit(false)
+    } else {
+      setSubmit(true)
+    }
+  }, [error])
 
   return (
     <Container maxWidth="sm">
@@ -57,39 +80,48 @@ export const Register = () => {
         <FormControl>
           <TextField
             required
-            label="Email"
+            label={error.email === '' ? 'Email' : error.email}
             name="email"
             type="text"
             value={data.email}
             onChange={handleOnChange}
+            color={
+              error.email === ''
+                ? "primary"
+                : "secondary"
+            }
           />
-          <TextField
+           <TextField
             required
-            label="Password"
+            label={error.password === '' ? 'Password' : error.password}
             name="password"
             type="password"
             value={data.password}
             onChange={handleOnChange}
+            color={
+              error.password === ''
+                ? "primary"
+                : "secondary"
+            }
           />
           <TextField
             required
-            label="Repeat Password"
+            label={error.passwordValidate === '' ? 'Repeat password' : error.passwordValidate}
             name="passwordValidate"
             type="password"
             value={data.passwordValidate}
             onChange={handleOnChange}
             color={
-              data.password === data.passwordValidate ||
-              data.passwordValidate === ""
+              error.passwordValidate ===  ""
                 ? "primary"
                 : "secondary"
             }
           />
 
-          <Button type="submit" variant="contained" color="primary">
+          <Button type="submit" variant="contained" color="primary" disabled={!submit} >
             Sing up
           </Button>
-          <Button onClick={back} variant="contained" color="primary">
+          <Button onClick={back} variant="contained" color="primary" >
             Back
           </Button>
         </FormControl>
