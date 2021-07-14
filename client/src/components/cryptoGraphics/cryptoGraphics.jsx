b9f01cf4f5e9a44263e9af140ce701bf68a675dd
Facundo Vaena <facundovaena@gmail.com>
2021-07-14 10:07:55 -0300

    refactor(client/src/components/cryptoGraphics):commented code deleted

--- client/src/components/cryptoGraphics/cryptoGraphics.jsx
+++ client/src/components/cryptoGraphics/cryptoGraphics.jsx
@@ -5,11 +5,9 @@ import { Container, Typography, Grid, Card } from '@material-ui/core';
 import SkewLoader from 'react-spinners/SkewLoader';
 
 import btcIcon from './cryptoIcons/bitcoin.png'
-//import ethIcon from './cryptoIcons/ethereum.png'
 import ethIcon from './cryptoIcons/ethereum2.png'
 import chzIcon from './cryptoIcons/chili.jpg'
 import filIcon from './cryptoIcons/filecoin.png'
-//import adaIcon from './cryptoIcons/ada.png' 
 import adaIcon from './cryptoIcons/ada2.png'
 import bnbIcon from './cryptoIcons/binance.png'
 
@@ -47,7 +45,7 @@ export const CryptoGraphics = () => {
       adaSocket = new WebSocket('wss://stream.binance.com:9443/ws/adausdt@trade')
       bnbSocket = new WebSocket('wss://stream.binance.com:9443/ws/bnbusdt@trade')
       
-      // erros handlers USD
+      // websockets errors handlers 
       ethSocket.onerror = (event) => { console.log(event) }
       btcSocket.onerror = (event) => { console.log(event) }
       chzSocket.onerror = (event) => { console.log(event) }
@@ -71,106 +69,64 @@ export const CryptoGraphics = () => {
 
 
 
-  useEffect(() => { //TODO: close useEffect
-    // ethSocket.onopen = () => { alert('arrancó') }
-    // ethSocket.onclose = () => { alert('me fui') };
+  useEffect(() => { 
+    
     ethSocket.onmessage = (event) => {
       let dataEth = JSON.parse(event.data);
       updateQuote(dataEth, eth, setEth);
-      // return () => { WebSocket.close('reasons') }
+      
     }
     bnbSocket.onmessage = (event) => {
       let dataBnb = JSON.parse(event.data);
       updateQuote(dataBnb, bnb, setBnb);
-      // return () => { WebSocket.close('reasons') }
+      
     }
 
     btcSocket.onmessage = (event) => {
       let dataBtc = JSON.parse(event.data);
       updateQuote(dataBtc, btc, setBtc);
-      // return () => { WebSocket.close('reasons') }
+      
     }
 
     adaSocket.onmessage = (event) => {
       let dataAda = JSON.parse(event.data);
       updateQuote(dataAda, ada, setAda);
-      // return () => { WebSocket.close('reasons') }
+      
     }
 
     chzSocket.onmessage = (event) => {
       let dataChz = JSON.parse(event.data);
       updateQuote(dataChz, chz, setChz);
-      // return () => { WebSocket.close('reasons') }
+      
     }
 
     filSocket.onmessage = (event) => {
       let dataFil = JSON.parse(event.data);
       updateQuote(dataFil, fil, setFil);
-      // return () => { WebSocket.close('reasons') }
+      
     }
-
-
-
-
-    // ethSocket.close()
-    // btcSocket.close()
-    // filSocket.close()
-    // adaSocket.close()
-    // bnbSocket.close()
-    //   ethSocket.onclose = () =>{};
-    //   ethSocket.onclose = () =>{};
-    //   btcSocket.onclose = () =>{}
-    //   btcSocket.onclose = () =>{}
-    //   filSocket.onclose = () =>{}
-    //   adaSocket.onclose = () =>{}
-    //   bnbSocket.onclose = () =>{}
 
 
   });
 
-  // websocket.onclose = function(evt) {
-  //         onClose(evt)
-  //      };
+  
 
   useEffect(() => {
-    // ethSocket.onclose = (evt) =>{
-    // ethSocket.close(evt)
-    // }
-    // btcSocket.onclose = (evt) =>{
-    //   btcSocket.close(evt)
-    // }
-    // filSocket.onclose = (evt) =>{
-    //   filSocket.close(evt)
-    // }
-    // adaSocket.onclose = (evt) =>{
-    //   adaSocket.close(evt)
-    // }
-    // bnbSocket.onclose = (evt) =>{
-    //   bnbSocket.close(evt)
-    // }
-
-
+    
     return () => {
 
       setConnection(false);
 
-      // alert('componente desmontado')
     }
   }, [])
 
-  // setEth({})
-  // setBtc({})
-  // setChz({})
-  // setFil({})
-  // setAda({})
-  // setBnb({})
+  
 
 
 
-  // UPDATE DATA COINS (info, prev info and color)
+  // UPDATE DATA COINS (info, coinState and setCoinState)
   const updateQuote = (dato, coin, setCoin) => {
     let numberPrice = parseFloat(dato.p);
-    // .toFixed(4) => String return
     setCoin({ ...coin, prevPrice: numberPrice })
     if (coin.prevPrice > coin.price) {
       return setCoin({ ...coin, price: numberPrice, color: 'major' })
