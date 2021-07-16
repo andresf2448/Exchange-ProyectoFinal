0ba617faca9a432ce18b265f292a9479b39a8d9a
Facundo Vaena <facundovaena@gmail.com>
2021-07-16 17:18:53 -0300

    refactor(client/src/components/TradingView & balance & offer): tradingView added to balance, offer component size modified

--- client/src/components/balance/balance.jsx
+++ client/src/components/balance/balance.jsx
@@ -1,15 +1,22 @@
-import {Container} from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import ManageBuyOffer from '../../methodsWallet/manageBuyOffer.jsx'
 // import BalanceAccount from '../../methodsWallet/balanceAccount'
 import Orderbook from 'methodsWallet/orderbook.jsx';
 import TradingView from 'components/tradingView/tradingView.jsx';
 
-export const Balance = ()=>{
+export const Balance = () => {
 
     return (
         <Container>
             <ManageBuyOffer />
-            <Orderbook />
+            <Grid container sm={12} justifyContent='space-between'>
+                <Grid item sm={3}>
+                    <Orderbook />
+                </Grid>
+                <Grid item sm={9}>
+                    <TradingView />
+                </Grid>
+            </Grid>
         </Container>
     )
 }
