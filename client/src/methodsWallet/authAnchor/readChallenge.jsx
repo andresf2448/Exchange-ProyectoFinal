19625a1cb11882ec1329096581d6f2c33bd8148f
rodrigo <juarezr@gmail.com>
2021-07-17 00:46:43 -0300

    feat(client/methodsWallet/authAnchor): add operation read challenge from de anchor

---
+++ client/src/methodsWallet/authAnchor/readChallenge.jsx
@@ -0,0 +1,37 @@
+import { Utils } from "stellar-sdk";
+import axios from 'axios';
+
+ const startAuthentication = async ({
+  authEndpoint,
+  serverSigningKey,
+  publicKey,
+  homeDomain,
+}) => {
+  const params = { account: publicKey, homeDomain };
+ 
+  const authURL = new URL(authEndpoint);
+  /* 
+  Object.entries(params).forEach(([key, value]) => {
+    authURL.searchParams.append(key, value);
+  }); */
+
+  const result = await axios.get(authURL.toString());
+ 
+
+  if (!result.transaction) {
+    throw new Error("The response didn’t contain a transaction");
+  }
+
+  const { tx } = Utils.readChallengeTx(
+    result.transaction,
+    serverSigningKey,
+    resultJson.network_passphrase,
+    homeDomain,
+    authURL.host,
+  );
+
+  return tx;
+};
+
+
+export default start;
