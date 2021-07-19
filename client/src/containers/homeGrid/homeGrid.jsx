e49b471153f5709db6dd5504aedde0d2f91ccc2f
MJULIAN90 <michaeljulian50@gmail.com>
2021-07-18 20:51:17 -0500

    Fix integration

--- client/src/containers/homeGrid/homeGrid.jsx
+++ client/src/containers/homeGrid/homeGrid.jsx
@@ -1,36 +1,52 @@
-import {Container, Grid} from '@material-ui/core';
-import {CryptoGraphics} from 'components/cryptoGraphics/cryptoGraphics';
+import { Container, Grid } from "@material-ui/core";
+import { CryptoGraphics } from "components/cryptoGraphics/cryptoGraphics";
 // import { Twitter } from 'components/twitter/twitter';
 // import {AboutPreview} from 'components/aboutPreview/aboutPreview';
 // import {Statistics} from 'components/statistics/statistics';
 // import {CryptoCalculator} from 'components/cryptoCalculator/cryptoCalculator';
 
-
-export const HomeGrid= ()=>{
-    return (
-        <Container maxWidth="lg">
-            <Grid container className="home-container" spacing={2} justifyContent="center">
-                <Grid item xs={12} container spacing={2} justifyContent="center" alignContent="space-around">
-                    <Grid item className="crypto-graphics" xs={12}>
-                        <CryptoGraphics/>
-                    </Grid>
-                    <Grid container className="home-container_boxLeft--bottom" spacing={2}>
-                        <Grid item xs={12}>
-                            {/* <Twitter /> */}
-                        </Grid>
-                    </Grid>
-                </Grid>
-<<<<<<< HEAD
-=======
-                <Grid item style={{ width: "100%" }} className="crypto-calculator" xs={3}>
-                    <CryptoCalculator/>
-                    
-                </Grid>
->>>>>>> 3ec08548641caddac086f0b11a3c9045b0ff0711
+export const HomeGrid = () => {
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        className="home-container"
+        spacing={2}
+        justifyContent="center"
+      >
+        <Grid
+          item
+          xs={12}
+          container
+          spacing={2}
+          justifyContent="center"
+          alignContent="space-around"
+        >
+          <Grid item className="crypto-graphics" xs={12}>
+            <CryptoGraphics />
+          </Grid>
+          <Grid
+            container
+            className="home-container_boxLeft--bottom"
+            spacing={2}
+          >
+            <Grid item xs={12}>
+              {/* <Twitter /> */}
             </Grid>
-        </Container>
-    )
-}
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ width: "100%" }}
+          className="crypto-calculator"
+          xs={3}
+        >
+          <CryptoCalculator />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 //    {/* <Grid item style={{ width: "100%" }} className="crypto-calculator" xs={3}>
 //                 <CryptoCalculator/>
