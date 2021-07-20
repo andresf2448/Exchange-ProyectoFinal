4ac33f835996a625ea42e3ac1ae829cee505e836
aguirrejuanse <you@example.com>
2021-07-20 10:07:30 -0300

    borre let de prueba

--- api/src/routes/toml/toml.js
+++ api/src/routes/toml/toml.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const toml = require('toml');
 const fs = require('fs');
 
-const stellar = require('../payment/payment')
-
 router.get('/', (req, res, next) => {
     const config = toml.parse(fs.readFileSync('./src/routes/toml/stellar.toml','utf-8'));
     res.send(config);
