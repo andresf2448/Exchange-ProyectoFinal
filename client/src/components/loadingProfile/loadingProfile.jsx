88777f59691cc01d108f210c5b6e89e9ad11f445
aguirrejuanse <you@example.com>
2021-07-20 19:08:22 -0300

    feat(/setting) agregue condicional hasProfile a loadingProfile

--- client/src/components/loadingProfile/loadingProfile.jsx
+++ client/src/components/loadingProfile/loadingProfile.jsx
@@ -37,6 +37,7 @@ export const LoadingProfile = () => {
     gender: "",
   });
 
+  
   function handleOnChange(event) {
     setData({
       ...data,
@@ -60,7 +61,7 @@ export const LoadingProfile = () => {
       occupation,
       gender,
     } = data;
-
+    
     await supabase.from("UserAnchor").insert([
       {
         id_user,
@@ -73,6 +74,11 @@ export const LoadingProfile = () => {
       },
     ]);
 
+    await supabase
+      .from("RegisteredUsers")
+      .update({ hasProfileUserAnchor: "true" })
+      .match({ id_user });
+    
     setData({
       firstName: "",
       lastName: "",
@@ -83,7 +89,19 @@ export const LoadingProfile = () => {
     });
   }
 
+  const [hasProfile, setHasProfile] = useState(null);
+
+  async function hasProfileFunction() {
+    let hasP  = await supabase
+  .from('RegisteredUsers')
+  .select('hasProfileUserAnchor')
+  .eq("id_user", session.user.id);
+  if(hasP.data[0].hasProfileUserAnchor === true) setHasProfile(true);
+  // console.log(hasP.data[0].hasProfileUserAnchor);
+  }
+
   useEffect(() => {
+    hasProfileFunction();
     if (error.isError) {
       setSubmit(false);
     } else {
@@ -96,103 +114,110 @@ export const LoadingProfile = () => {
       <Typography variant="h4" gutterBottom>
         Update Information
       </Typography>
-      <form onSubmit={updateProfile}>
-        <Grid container>
-          <Grid
-            container
-            item
-            sm={6}
-            direction="column"
-            alignContent="space-around"
-          >
-            <TextField
-              label={error.firstName === "" ? "First Name" : error.firstName}
-              required
-              name="firstName"
-              type="text"
-              value={data.firstName}
-              onChange={handleOnChange}
-              color={error.firstName === "" ? "primary" : "secondary"}
-            />
-
-            <TextField
-              label={error.lastName === "" ? "Last Name" : error.lastName}
-              required
-              name="lastName"
-              type="text"
-              value={data.lastName}
-              onChange={handleOnChange}
-              color={error.lastName === "" ? "primary" : "secondary"}
-            />
-
-            <TextField
-              label="Additional Name"
-              name="additionalName"
-              type="text"
-              value={data.additionalName}
-              onChange={handleOnChange}
-            />
-            <TextField
-              label="Mobile Number"
-              name="mobileNumber"
-              type="text"
-              value={data.mobileNumber}
-              onChange={handleOnChange}
-            />
-
-            <TextField
-              label="Occupation"
-              name="occupation"
-              type="text"
-              value={data.occupation}
-              onChange={handleOnChange}
-            />
-          </Grid>
-
-          <Grid
-            container
-            item
-            sm={6}
-            direction="column"
-            alignContent="space-around"
-          >
-            <FormControl component="fieldset">
-              <FormLabel component="legend">
-                {error.gender === "" ? "Gender" : error.gender}
-              </FormLabel>
-              <RadioGroup aria-label="gender" name="gender">
-                <FormControlLabel
-                  value="Male"
-                  onClick={handleOnChange}
-                  control={<Radio />}
-                  label="Male"
-                />
-                <FormControlLabel
-                  value="Female"
-                  onClick={handleOnChange}
-                  control={<Radio />}
-                  label="Female"
-                />
-                <FormControlLabel
-                  value="Other"
-                  onClick={handleOnChange}
-                  control={<Radio />}
-                  label="Other"
-                />
-              </RadioGroup>
-            </FormControl>
-            <Button
-              type="submit"
-              disabled={!submit}
-              variant="contained"
-              color="primary"
-              onClick={updateProfile}
+      {hasProfile ?
+        <Container>
+          <Typography variant="h4" gutterBottom>Completaste tu perfil</Typography>
+          <Button onClick={() => setHasProfile(false)}>Editar</Button>
+        </Container>
+        :
+        <form onSubmit={updateProfile}>
+          <Grid container>
+            <Grid
+              container
+              item
+              sm={6}
+              direction="column"
+              alignContent="space-around"
             >
-              {"Send "}
-            </Button>
+              <TextField
+                label={error.firstName === "" ? "First Name" : error.firstName}
+                required
+                name="firstName"
+                type="text"
+                value={data.firstName}
+                onChange={handleOnChange}
+                color={error.firstName === "" ? "primary" : "secondary"}
+              />
+
+              <TextField
+                label={error.lastName === "" ? "Last Name" : error.lastName}
+                required
+                name="lastName"
+                type="text"
+                value={data.lastName}
+                onChange={handleOnChange}
+                color={error.lastName === "" ? "primary" : "secondary"}
+              />
+
+              <TextField
+                label="Additional Name"
+                name="additionalName"
+                type="text"
+                value={data.additionalName}
+                onChange={handleOnChange}
+              />
+              <TextField
+                label="Mobile Number"
+                name="mobileNumber"
+                type="text"
+                value={data.mobileNumber}
+                onChange={handleOnChange}
+              />
+
+              <TextField
+                label="Occupation"
+                name="occupation"
+                type="text"
+                value={data.occupation}
+                onChange={handleOnChange}
+              />
+            </Grid>
+
+            <Grid
+              container
+              item
+              sm={6}
+              direction="column"
+              alignContent="space-around"
+            >
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  {error.gender === "" ? "Gender" : error.gender}
+                </FormLabel>
+                <RadioGroup aria-label="gender" name="gender">
+                  <FormControlLabel
+                    value="Male"
+                    onClick={handleOnChange}
+                    control={<Radio />}
+                    label="Male"
+                  />
+                  <FormControlLabel
+                    value="Female"
+                    onClick={handleOnChange}
+                    control={<Radio />}
+                    label="Female"
+                  />
+                  <FormControlLabel
+                    value="Other"
+                    onClick={handleOnChange}
+                    control={<Radio />}
+                    label="Other"
+                  />
+                </RadioGroup>
+              </FormControl>
+              <Button
+                type="submit"
+                disabled={!submit}
+                variant="contained"
+                color="primary"
+                onClick={updateProfile}
+              >
+                {"Send "}
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </form>
+        </form>
+      }
     </Container>
   );
 };
