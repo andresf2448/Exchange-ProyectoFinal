9f0ec3bd104a3edd16f12cc4c130d8e665c731e8
Javi <javiercastroloureiro@gmail.com>
2021-07-22 21:03:48 -0300

    feat(includesStripe): add stripe to transaction

--- client/src/methodsWallet/transactionsFlow.jsx
+++ client/src/methodsWallet/transactionsFlow.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getClientSecret } from "redux/actions/actions";
 import { supabase } from "supabase/supabase";
+import CheckoutForm from '../components/stripe/checkoutForm'
 
 export default function TransactionsPopup() {
+
+  const dispatch = useDispatch()
+
   const [transactionType, setTransactionType] = useState();
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [email, setEmail] = useState();
+  // const [firstName, setFirstName] = useState();
+  // const [lastName, setLastName] = useState();
+  // const [email, setEmail] = useState();
+  const [input, setInput] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    currency: '',
+    amount: ''
+  })
    const [ error, setError] = useState();
   /*const [transaction, setTransaction] = useState(); */
   const [kyc, setKyc] = useState(false);
@@ -20,10 +33,11 @@ export default function TransactionsPopup() {
       .from("transactions")
       .select("*")
       .eq("id", id);
-    
+    console.log('El errorrrrrrr', error)
+    console.log('Dataaaaaa', data)
     if (error) return setError(true);
     if (data[0]) {
-      
+      console.log('La dataaaaaaaa', data[0].kind)
       return setTransactionType(data[0].kind);
     }
   };
@@ -37,42 +51,85 @@ export default function TransactionsPopup() {
     })
     .subscribe(); */
 
-  async function handleSubmit() {
-     await supabase
-      .from("transactions")
-      .update([
-        {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          kyc_verified: true,
-        },
-      ])
-      .eq("id", id);
-    
-    return setKyc(true);
+    const getPublicKey = async () => {
+      const { data, error } = await supabase
+      .from('datauser')
+      .select('public_key')
+      .eq('id_user', id)
+
+      if(error) return error
+      return data
+    }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    let data = await getPublicKey()
+    if(data[0].public_key) {
+      let supa = await supabase
+        .from("transactions")
+        .insert([
+          {
+            firstName: input.firstName,
+            lastName: input.lastName,
+            email: input.email,
+            amount_in: input.amount,
+            kyc_verified: true,
+            id: id,
+            account_id: data[0].public_key
+          }
+        ])
+        // .match("id", id);
+        console.log('Supaaaaa', supa)
+        dispatch(getClientSecret({currency: input.currency, amount: input.amount}))
+      
+      setKyc(true);
+
+    } else {
+      setError(data)
+    }
   }
+  console.log('Transaction type', transactionType)
   return (
     <div>
       <div> Hola </div>
 
-      {!firstName && !lastName && !email && !kyc && (
-        <form onSubmit={() => handleSubmit()}>
+      {!kyc && (
+        <form onSubmit={handleSubmit}>
          
           <input
             type="text"
             placeholder="firts name"
-            onChange={(event) => setFirstName(event)}
+            name="firtsName"
+            onChange={(event) => setInput({...input, [event.target.name]: event.target.value})}
           />
           <input
             type="text"
             placeholder="last name"
-            onChange={(event) => setLastName(event)}
+            name="lastName"
+            onChange={(event) => setInput({...input, [event.target.name]: event.target.value})}
           />
           <input
             type="email"
             placeholder="email"
-            onChange={(event) => setEmail(event)}
+            name="email"
+            onChange={(event) => setInput({...input, [event.target.name]: event.target.value})}
+          />
+          <input
+            type="text"
+            placeholder="currency"
+            name="currency"
+            onChange={(event) => setInput({...input, [event.target.name]: event.target.value})}
+          />
+          <input
+            type="text"
+            placeholder="amount"
+            name="amount"
+            onChange={(event) => setInput({...input, [event.target.name]: event.target.value})}
+          />
+          <input
+            type="submit"
+            value='Send'
+            // onChange={(event) => setEmail(event)}
           />
         </form>
       )}
@@ -81,7 +138,7 @@ export default function TransactionsPopup() {
        
         {transactionType === "deposit" && kyc && (
           <div>
-            ACA IRIA EL COMPONENTE DE STRIPE PARA QUE NOS DEPOSITE
+            <CheckoutForm/>
           </div>
         )}
         {transactionType === "withdraw" && kyc && (
