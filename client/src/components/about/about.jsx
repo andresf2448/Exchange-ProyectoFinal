4d95d0d3173a3bf7060d6478188dd008a7eeb120
Facundo Vaena <facundovaena@gmail.com>
2021-07-25 14:37:43 -0300

    refactor(client/src/components/about):title centered

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -15,7 +15,7 @@ export default function About () {
 
     return (
         <Grid container style={{ display: 'flex', flexDirection: 'column', width:'90%', marginLeft:'86px' }}>
-            <Grid item style={{ marginRight: '80px', marginBottom: '20px' }}>
+            <Grid item style={{ textAlign:'center', marginRight:'200px',marginBottom: '20px' }}>
                 <Typography variant='h3'>Meet The Team</Typography>
             </Grid>
             <Grid item style={{display:'flex' ,flexWrap:'wrap', gap:'10px'}}>
@@ -43,40 +43,4 @@ export default function About () {
     )
 }
 
-{/* <Grid item style={{ gap: '20px', marginLeft: '60px', display: 'flex', flexWrap: 'wrap' }} >
-                <Card style={{ width: '200px', padding: '20px' }}>
-                    <CardContent>
-                        <CardMedia
-                            component="img"
-                            alt="Contemplative Reptile"
-                            height="140"
-                            image="https://t3.ftcdn.net/jpg/03/91/19/22/360_F_391192211_2w5pQpFV1aozYQhcIw3FqA35vuTxJKrB.jpg"
-                            title="Contemplative Reptile"
-                        />
-                        <Typography variant='h6'>
-                            Rodrigo Juarez
-                        </Typography>
 
-                    </CardContent>
-                    {/* <CardActions>
-                    <Button><NavLink to='/faq' style={{ textDecoration: 'none' }}>Learn more</NavLink></Button>
-                </CardActions> */}
-                // </Card >
-    // <Card style={{ width: '200px', padding: '20px' }}>
-    //     <CardContent>
-    //         <CardMedia
-    //             component="img"
-    //             alt="Contemplative Reptile"
-    //             height="140"
-    //             image="https://t3.ftcdn.net/jpg/03/91/19/22/360_F_391192211_2w5pQpFV1aozYQhcIw3FqA35vuTxJKrB.jpg"
-    //             title="Contemplative Reptile"
-    //         />
-    //         <Typography variant='h6'>
-    //             Rodrigo Juarez
-    //         </Typography>
-
-    //     </CardContent>
-    //     {/* <CardActions>
-    //                 <Button><NavLink to='/faq' style={{ textDecoration: 'none' }}>Learn more</NavLink></Button>
-    //             </CardActions> */}
-    // </Card> * /}
