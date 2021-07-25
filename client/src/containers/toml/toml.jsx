f771d5996e2b5f876db397c467719ca1c8cdae69
rodrigo <juarezr@gmail.com>
2021-07-25 08:39:35 -0300

    featclient(/orderbook): add multiple assets for orderbook

--- client/src/containers/toml/toml.jsx
+++ client/src/containers/toml/toml.jsx
@@ -17,20 +17,3 @@ const Toml = () => {
 };
 
 export default Toml;
-{/* <Container className={classes.toml}>
-            {toml === undefined ?
-                <Typography variant="h3"> Loading...</Typography>
-                :
-                    <Container>
-                        <Typography align="left" variant="body1">Version = {toml.VERSION}</Typography>
-                        <Typography align="left" variant="body1">Accounts = {toml.ACCOUNTS[0]}</Typography>
-                        <Typography align="left" variant="body1">ORG_NAME = {toml.DOCUMENTATION.ORG_NAME}</Typography>
-                        <Typography align="left" variant="body1">ORG_DBA = {toml.DOCUMENTATION.ORG_DBA}</Typography>
-                        <Typography align="left" variant="body1">ORG_URL = {toml.DOCUMENTATION.ORG_URL}</Typography>
-                        <Typography align="left" variant="body1">ORG_LOGO = {toml.DOCUMENTATION.ORG_LOGO}</Typography>
-                        <Typography align="left" variant="body1">ORG_DESCRIPTION = {toml.DOCUMENTATION.ORG_DESCRIPTION}</Typography>
-                        <Typography align="left" variant="body1">ORG_PHYSICAL_ADDRESS = {toml.DOCUMENTATION.ORG_PHYSICAL_ADDRESS}</Typography>
-                        <Typography align="left" variant="body1">ORG_OFFICIAL_EMAIL = {toml.DOCUMENTATION.ORG_OFFICIAL_EMAIL}</Typography>
-                    </Container>
-            }
-        </Container> */}
