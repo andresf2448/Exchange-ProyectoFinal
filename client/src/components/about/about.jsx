1bf4cc7777ef84c9f657f9631a0e019968a55548
Facundo Vaena <facundovaena@gmail.com>
2021-07-26 11:33:54 -0300

    refactor(client/src/components/about & aboutCard & about.css):about styles improved

--- client/src/components/about/about.jsx
+++ client/src/components/about/about.jsx
@@ -1,27 +1,24 @@
-import React, { useState } from 'react'
-import { Typography, Card, CardContent, CardMedia, Grid } from '@material-ui/core';
+import React from 'react'
+import { Typography, Grid } from '@material-ui/core';
 // import './about.css';
 import AboutCard from './aboutCard'
 
 export default function About() {
 
-//https://www.linkedin.com/in/nabilelsawi/detail/photo/
-    let team = [{name:'Rodrigo Juarez', img: 'https://avatars.githubusercontent.com/u/64714468?v=4'}, {name:'Martín Bobrik', img: 'https://avatars.githubusercontent.com/u/13581554?v=4'}, {name:'Javier Castro', img: 'https://avatars.githubusercontent.com/u/69270095?v=4'},
-        {name: 'Andrés Velásquez', img: 'https://avatars.githubusercontent.com/u/58791994?v=4'},{name:'Juan Aguirre', img: 'https://avatars.githubusercontent.com/u/63589306?v=4'} ,{name:'Facundo Betella', img: 'https://avatars.githubusercontent.com/u/76120904?v=4'}, {name:'Julián Ruiz', img: 'https://www.nationalgeographic.com.es/medio/2021/03/09/perro_4da5a8be_800x1200.jpg'},
-        , {name:'Facundo Vaena', img: 'https://avatars.githubusercontent.com/u/79978628?v=4'}]
+    let team = [{ name: 'Rodrigo Juarez', img: 'https://avatars.githubusercontent.com/u/64714468?v=4' }, { name: 'Martín Bobrik', img: 'https://avatars.githubusercontent.com/u/13581554?v=4' }, { name: 'Javier Castro', img: 'https://avatars.githubusercontent.com/u/69270095?v=4' },
+    { name: 'Andrés Velásquez', img: 'https://avatars.githubusercontent.com/u/58791994?v=4' }, { name: 'Juan Aguirre', img: 'https://avatars.githubusercontent.com/u/63589306?v=4' }, { name: 'Facundo Betella', img: 'https://avatars.githubusercontent.com/u/76120904?v=4' }, { name: 'Julián Ruiz', img: 'https://www.nationalgeographic.com.es/medio/2021/03/09/perro_4da5a8be_800x1200.jpg' }, { name: 'Facundo Vaena', img: 'https://avatars.githubusercontent.com/u/79978628?v=4' }]
 
-    const ejemplo = () => alert('hola')
     return (
-        <Grid container  style={{display:'flex', flexDirection:'column', width:'90%', marginLeft:'86px'}}>
-            <Grid item style={{textAlign:'center', marginRight:'200px', marginBottom:'20px'}}>
+        <Grid container style={{ display: 'flex', flexDirection: 'column', width: '90%', marginLeft: '86px' }} sm='12'>
+            <Grid item style={{ textAlign: 'center', justifyContent: 'center', marginRight: '90px' }}>
                 <Typography variant='h3'>Meet The Team</Typography>
             </Grid>
-            <Grid item style={{display:'flex'}} >
+            <Grid container style={{ display: 'flex', marginTop: '-80px' }} sm={12}>
                 {team.map(element => {
 
-                    return (<div> 
-                    <AboutCard name={element.name} img={element.img}/>
-                    </div>)
+                    return (<Grid item sm={1} style={{ margin: '6%', marginBottom: '16%' }}>
+                        <AboutCard name={element.name} img={element.img} />
+                    </Grid>)
                 })}
             </Grid>
         </Grid>
--- client/src/components/about/aboutCard.jsx
+++ client/src/components/about/aboutCard.jsx
@@ -5,46 +5,47 @@ export default function AboutCard({ name, img }) {
 
     const [isRotated, setIsRotated] = useState(false);
 
-    const onRotate = () => {setIsRotated((rotated) => !rotated)};
+    const onRotate = () => { setIsRotated((rotated) => !rotated) };
 
 
     return (
-        <Grid container className={`card ${isRotated ? 'rotated' : ''}`} onClick={onRotate}>
-            <Card className='front' style={{width:'200px', padding:'20px'}} >
-                        <CardContent >
-                            <CardMedia
-                                
-                                component="img"
-                                alt="Image"
-                                height="140"
-                                image="https://t3.ftcdn.net/jpg/03/91/19/22/360_F_391192211_2w5pQpFV1aozYQhcIw3FqA35vuTxJKrB.jpg"
-                                title="Team Member"
-                            />
-                            <Typography variant='h6'>
-                                {name}
-                            </Typography>
+        <Grid container className={`card ${isRotated ? 'rotated' : ' '}`} onClick={onRotate} sm={12}>
+            <Card className='front' style={{ width: '200px', height:'260px',  }} sm={2}>
+                <CardContent >
+                    <CardMedia
+                        style={{ borderRadius: '20px' }}
+                        component="img"
+                        alt="Image"
+                        height="140"
+                        image="https://t3.ftcdn.net/jpg/03/91/19/22/360_F_391192211_2w5pQpFV1aozYQhcIw3FqA35vuTxJKrB.jpg"
+                        title="Team Member"
+                    />
+                    <Typography variant='h6' style={{textAlign:'center'}}>
+                        {name}
+                    </Typography>
 
-                        </CardContent>
+                </CardContent>
 
-                    </Card>
-                    {/*-------- */}
-                    <Card className='back' style={{width:'200px', padding:'20px'}}>
-                        <CardContent>
-                            <CardMedia
-                                className={`card ${isRotated ? 'rotated' : ''}`}
-                                component="img"
-                                alt="Image"
-                                height="140"
-                                image={img}
-                                title="Team Member"
-                            />
-                            <Typography variant='h6'>
-                                {name}
-                            </Typography>
+            </Card>
+            {/*-------- */}
+            <Card className='back' style={{ width: '200px', height:'260px' }}>
+                <CardContent>
+                    <CardMedia
+                        
+                        style={{borderRadius:'20px'}}
+                        component="img"
+                        alt="Image"
+                        height="140"
+                        image={img}
+                        title="Team Member"
+                    />
+                    <Typography variant='h6' style={{textAlign:'center'}}>
+                        {name}
+                    </Typography>
 
-                        </CardContent>
+                </CardContent>
 
-                    </Card>
+            </Card>
         </Grid>
     )
 }
