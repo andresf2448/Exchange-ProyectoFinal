52b9a7622cc34db185fb931ad0d37b5cbc1394aa
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-27 00:32:40 -0700

    testing

--- api_twitter/index.js
+++ api_twitter/index.js
@@ -1,5 +1,5 @@
 const server = require("./src/App");
 
 module.exports = server.listen(process.env.PORT, () => {
-  console.log(`%s listening at ${process.env.PORT} twitter`); // eslint-disable-line no-console
+  console.log(`%s listening at twitter`); // eslint-disable-line no-console
 });
