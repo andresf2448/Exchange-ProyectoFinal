e05430ff600c7c9865238b85d10d1334396c1b64
Facundo <facundobettella@outlook.com>
2021-07-27 10:44:59 -0300

    refactor(styles):in progress

--- client/src/components/showUserData/showUserData.jsx
+++ client/src/components/showUserData/showUserData.jsx
@@ -1,16 +1,17 @@
 import {  Grid, Typography } from "@material-ui/core";
 import { supabase } from "supabase/supabase";
+import useStyles from 'styles';
 
 
 export const ShowUserData = () => {
+  const classes = useStyles();
   let { user } = supabase.auth.session();
 
   let { email } = user;
 
   return (
-      <Grid container style={{display: 'flex'}}>
+      <Grid container style={{display: 'flex', paddingLeft: '1%'}}>
         <Typography>{email}</Typography>
       </Grid>
-
   );
 };
