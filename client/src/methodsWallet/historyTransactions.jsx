37dc5b91872279d9de4100dbd1bfb94ba57d66b2
Facundo <facundobettella@outlook.com>
2021-07-30 12:49:29 -0300

    styles(loading home/ transaction history/ landing buttons)

--- client/src/methodsWallet/historyTransactions.jsx
+++ client/src/methodsWallet/historyTransactions.jsx
@@ -1,6 +1,6 @@
 import StellarSdk from "stellar-sdk";
 import { useState } from "react";
-import {TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
+import {Grid, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
 import useStyles from "styles";
 // import Transaction from "components/transaction/transaction";
 
@@ -30,6 +30,12 @@ export default function HistoryTransactions({ publicKey }) {
       {/* <Divider variant="middle" className={classes.divider} /> */}
       {/* style={{ overflowY: "scroll", height: "40vh" }} */}
       {/* <Grid align="center"  > */}
+
+
+     
+        {
+
+        transactions ?
         <TableContainer className={classes.adminTableContainer} style={{ marginBottom: '2vh', marginTop:'2vh' }}>
           <Table className={classes.adminTable} stickyHeader>
             <TableHead>
@@ -48,44 +54,14 @@ export default function HistoryTransactions({ publicKey }) {
             </TableBody>
           </Table>
         </TableContainer>
-        {/* <ul style={{ listStyle: "none", textAlign: "left" }}>
-          {transactions &&
-            transactions.map((transaction, i) => (
-              <li key={transaction.id}>
-                {i} - id de transaction: {transaction.id}
-              </li>
-            ))}
-        </ul> */}
-      {/* </Grid> */}
+        :
+        <Grid>
+          <Typography variant="h4">
+            You haven´t made transactions yet
+          </Typography>
+        </Grid>
+    }
     </div>
   );
 }
 
-{/* <TableContainer className={classes.adminTableContainer} style={{ marginBottom: '3vh', marginTop: '3vh' }}>
-  <Table stickyHeader className={classes.adminTable}>
-    <TableHead>
-      <TableRow>
-        <TableCell align='center'>Asset</TableCell>
-        <TableCell>Balance</TableCell>
-        <TableCell>Mount of selling offers</TableCell>
-        <TableCell>Mount of buying offers</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {account?.balances?.map(({ balance, asset_code, selling_liabilities, buying_liabilities }, index) => {
-        return (
-          <TableRow hover={{ backgroundColor: 'black' }}>
-            <TableCell align='center'>{!asset_code ? "XLM" : asset_code}</TableCell>
-            <TableCell align='center'>{parseFloat(balance).toFixed(2)}</TableCell>
-            <TableCell align='center'>{parseFloat(selling_liabilities).toFixed(2)}</TableCell>
-            <TableCell align='center'>{parseFloat(buying_liabilities).toFixed(2)}</TableCell>
-          </TableRow>
-
-        )
-      })}
-    </TableBody>
-
-
-  </Table>
-
-</TableContainer> */}
