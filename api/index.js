2ea41be40d2338c89ea2dd1b4f03e0241f866822
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-07-30 05:31:46 -0700

    port

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const server = require("./src/app");
 
+
 // Here we put the main route and the secondary routes we have to create it in a new file and import its here.
 
-module.exports = server.listen(process.env.PORT, () => {
+module.exports = server.listen(process.env.PORT || 5000, () => {
     
   console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 });
