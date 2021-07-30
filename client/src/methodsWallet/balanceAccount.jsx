656f31eee53833743aa77710ab1e559aa09fb367
Facundo Vaena <facundovaena@gmail.com>
2021-07-30 09:39:09 -0300

    refactor(client/src/methodsWallet/balanceAccount):Previous balance replaced for a table

--- client/src/methodsWallet/balanceAccount.jsx
+++ client/src/methodsWallet/balanceAccount.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import StellarSdk from "stellar-sdk";
-import { Divider } from "@material-ui/core";
+import { Divider, TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
 import useStyles from "styles";
 import { supabase } from "../supabase/supabase";
 // import CreateAccount from "./createAccount";
@@ -24,7 +24,7 @@ export default function BalanceAccount() {
     if (data.length > 0) {
       setUser(() => true);
       getBalance()
-      
+
     }
   };
 
@@ -47,7 +47,35 @@ export default function BalanceAccount() {
         <div>
           {account ? (
             <div>
-              {account?.balances?.map(
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Asset</TableCell>
+                      <TableCell>Balance</TableCell>
+                      <TableCell>Mount of selling offers</TableCell>
+                      <TableCell>Mount of buying offers</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {account?.balances?.map(({ balance, asset_code, selling_liabilities, buying_liabilities }, index) => {
+                      return (
+                        <TableRow>
+                          <TableCell>{!asset_code ? "XLM" : asset_code}</TableCell>
+                          <TableCell>{parseFloat(balance).toFixed(2)}</TableCell>
+                          <TableCell>{parseFloat(selling_liabilities).toFixed(2)}</TableCell>
+                          <TableCell>{parseFloat(buying_liabilities).toFixed(2)}</TableCell>
+                        </TableRow>
+
+                      )
+                    })}
+                  </TableBody>
+
+
+                </Table>
+
+              </TableContainer>
+              {/* {account?.balances?.map(
                 (
                   {
                     balance,
@@ -58,16 +86,16 @@ export default function BalanceAccount() {
                   index
                 ) => (
                   <div key={index} className={classes.balanceAccount}>
-                    <Divider className={classes.divider} /> <br/>
+                    <Divider className={classes.divider} /> <br />
                     <div>Asset: {!asset_code ? "XLM" : asset_code} </div>
                     <br />
                     <br />
                     <div>Balance: {parseFloat(balance).toFixed(2)} </div>
                     <div> Mount of selling offers: {parseFloat(selling_liabilities).toFixed(2)}</div>
-                    <div> Mount of buying offers: {parseFloat(buying_liabilities).toFixed(2)}</div> <br/>
+                    <div> Mount of buying offers: {parseFloat(buying_liabilities).toFixed(2)}</div> <br />
                   </div>
                 )
-              )}
+              )} */}
             </div>
           ) : (
             <HashLoader color={"#ffd523"} size={30} />
