7bd057621cbfce1a315cf95f411c3ef7cf620ed9
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-08-06 03:03:38 -0700

    deleted files

--- client/src/components/loadingProfile/whatsapp.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default function sendCode(code, number) {
-  try {
-    axios(`/whatsapp?code=${code}&number=${number}`);
-  } catch (err) {
-    console.log(err)
-  }
-}
